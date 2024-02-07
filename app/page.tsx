40ffbd411afce45d4851d19bdf48ab3ae1b4fd15
fazzyA <faz.pak@gmail.com>
2024-02-07 13:36:16 +0500

    ej2 library

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,47 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { SpreadsheetComponent, SheetsDirective, SheetDirective, ColumnsDirective, RangesDirective, RangeDirective, RowsDirective, RowDirective, CellsDirective, CellDirective, CellStyleModel, ColumnDirective } from '@syncfusion/ej2-react-spreadsheet';
+import { defaultData } from './datasource';
 
-export default function Home() {
+export default function Home(this: any) {
+  let spreadsheet!: SpreadsheetComponent;
+  const boldRight: CellStyleModel = { fontWeight: 'bold', textAlign: 'right' };
+  const bold: CellStyleModel = { fontWeight: 'bold' };
+
+  function onCreated(): void {
+    spreadsheet.cellFormat({ fontWeight: 'bold', textAlign: 'center', verticalAlign: 'middle' }, 'A1:F1');
+    spreadsheet.numberFormat('$#,##0.00', 'F2:F31');
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    <>
+      <h2>Syncfusion React Spreadsheet Component</h2>
+      <SpreadsheetComponent openUrl='https://services.syncfusion.com/react/production/api/spreadsheet/open'
+        saveUrl='https://services.syncfusion.com/react/production/api/spreadsheet/save'
+        ref={(Obj) => { spreadsheet = Obj as SpreadsheetComponent }} created={onCreated.bind(this)} >
+        <SheetsDirective>
+          <SheetDirective name="Car Sales Report">
+            <RangesDirective>
+              <RangeDirective dataSource={defaultData}></RangeDirective>
+            </RangesDirective>
+            <RowsDirective>
+              <RowDirective index={30}>
+                <CellsDirective>
+                  <CellDirective index={4} value="Total Amount:" style={boldRight}></CellDirective>
+                  <CellDirective formula="=SUM(F2:F30)" style={bold}></CellDirective>
+                </CellsDirective>
+              </RowDirective>
+            </RowsDirective>
+            <ColumnsDirective>
+              <ColumnDirective width={180}></ColumnDirective>
+              <ColumnDirective width={130}></ColumnDirective>
+              <ColumnDirective width={130}></ColumnDirective>
+              <ColumnDirective width={180}></ColumnDirective>
+              <ColumnDirective width={130}></ColumnDirective>
+              <ColumnDirective width={120}></ColumnDirective>
+            </ColumnsDirective>
+          </SheetDirective>
+        </SheetsDirective>
+      </SpreadsheetComponent>
+    </>
+  )
 }
