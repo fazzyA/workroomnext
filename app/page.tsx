41e9bdc9a31cf301d952d44a4593f8719fb1b874
faizaGS <faiza@groupshop.com>
2024-03-08 16:56:23 +0500

    chatbot ui

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import { useCallback, useEffect, useState } from "react";
 import { Workbook, WorkbookInstance } from "@fortune-sheet/react";
 import "@fortune-sheet/react/dist/index.css";
 import { useRef } from "react";
-import SearchModal from "./components/SearchModal"; 
+import SearchModal from "./components/SearchModal";
 import Header from "./components/Header";
 import { Sheet } from "@fortune-sheet/core";
+import Chatbot from "./components/Chatbot";
 
-function Home (this: any) {
+function Home(this: any) {
   // const [isOpen, setIsOpen] = useState(false); 
-  const [isClicked, setIsClicked] = useState(false); 
+  const [isClicked, setIsClicked] = useState(false);
   const [newData, setNewData] = useState<any>([]);
   const ref = useRef<WorkbookInstance>(null);
 
@@ -18,6 +19,8 @@ function Home (this: any) {
     {
       id: "1",
       name: "sheet1",
+      row: 10,
+      column: 15,
       celldata: [{ r: 0, c: 0, v: { v: "1" } }],
       order: 0,
     },
@@ -26,16 +29,19 @@ function Home (this: any) {
     setData(d);
   }, []);
 
-//  useEffect()
-   console.log("....", data)
-   
+  //  useEffect()
+  console.log("....", data)
+
   return (
-    <div style={{height: "100vh"}}>
-        {/* <SearchModal setNewData={(data) => console.log("////", data)} isOpen={isOpen} onClose={() => setIsOpen(false)} setIsOpen={setIsOpen} /> */}
-        <Header setNewData={(rd: any)=>ref.current?.updateSheet([{  id: "1", name: "Sheet1", celldata: rd}])}/>
-        {/* <Workbook {...settings} /> */}
-        <Workbook ref={ref} data={data} onChange={onChange} />
-        {/* <div className="column column-25"><input type="text" /></div> */}
+    <div style={{ height: "100vh", display:"flex" }}>
+      <div style={{ flex: 1, marginRight: "10px", width:"70px" }}>
+      <Header setNewData={(rd: any) => ref.current?.updateSheet([{ id: "1", name: "Sheet1", celldata: rd }])} />
+      <Workbook ref={ref} data={data} onChange={onChange} />
+      </div>
+      {/* <div className="column column-25"><input type="text" /></div> */}
+      <div>
+      <Chatbot />
+      </div>
     </div>
   );
 }
